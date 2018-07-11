72e4f32ad42e1966d710a0ac3d8a27e3bbf321d5
Adam Brown <abrow191@mail.depaul.edu>
2018-07-10 21:09:49 -0500

    Create collection class ShapeList.

---
+++ src/model/persistence/ShapeList.java
@@ -0,0 +1,35 @@
+package model.persistence;
+
+import java.util.ArrayList;
+import java.awt.*;
+
+final public class ShapeList {
+
+    final private ArrayList sl;
+
+    public ShapeList(){
+        sl = new ArrayList<Shape>();
+    }
+
+    public boolean isEmpty(){
+        return sl.isEmpty();
+    }
+
+    public void addShape(Shape shape){
+        if(shape == null)
+            throw new IllegalArgumentException();
+        sl.add(shape);
+    }
+
+    public void removeShape(Shape shape){
+        if(!sl.contains(shape))
+            throw new IllegalArgumentException();
+        if(sl.isEmpty())
+            throw new IllegalStateException();
+        sl.remove(shape);
+    }
+
+    public int numShapes(){
+        return sl.size();
+    }
+}
