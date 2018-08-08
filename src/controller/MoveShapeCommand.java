98e3c6834170b0badabab574356a18c036dcf459
Adam Brown <abrow191@mail.depaul.edu>
2018-08-08 17:15:43 -0500

    Create MoveShapeCommand.

---
+++ src/controller/MoveShapeCommand.java
@@ -0,0 +1,53 @@
+package controller;
+
+import model.persistence.ApplicationState;
+import java.util.List;
+import java.util.ArrayList;
+import model.interfaces.IShape;
+import model.persistence.Point;
+import model.persistence.ShapeConfiguration;
+import model.ShapeType;
+import model.persistence.ShapeFactory;
+
+public class MoveShapeCommand implements ICommand {
+
+    private ApplicationState appState;
+
+    public MoveShapeCommand(ApplicationState appState){
+        this.appState = appState;
+    }
+
+    @Override
+    public void run() {
+
+        List<IShape> movedShapes = new ArrayList<IShape>();
+        List<IShape> shapesOnCanvas = appState.shapeList.getIterable();
+        List<IShape> currentlySelectedShapes = appState.selectedShapes.getIterable();
+        IShape shapeToMove = null;
+
+        for(IShape shape : currentlySelectedShapes){
+            int newStartX = shape.getStartPoint().getX() + 50;
+            int newStartY = shape.getStartPoint().getY() + 50;
+            int newEndX = shape.getEndPoint().getX() + 50;
+            int newEndY = shape.getEndPoint().getY() + 50;
+            Point newStartPoint = new Point(newStartX, newStartY);
+            Point newEndPoint = new Point(newEndX, newEndY);
+            ShapeConfiguration sc = shape.getShapeConfiguration();
+
+            if(shape.getShapeType() == ShapeType.ELLIPSE)
+                shapeToMove = ShapeFactory.createEllipse(newStartPoint, newEndPoint, sc);
+            else if(shape.getShapeType() == ShapeType.RECTANGLE)
+                shapeToMove = ShapeFactory.createRectangle(newStartPoint, newEndPoint, sc);
+            else if(shape.getShapeType() == ShapeType.TRIANGLE)
+                shapeToMove = ShapeFactory.createTriangle(newStartPoint, newEndPoint, sc);
+
+            movedShapes.add(shapeToMove);
+        }
+
+        for(IShape shape : currentlySelectedShapes)
+            appState.shapeList.removeShape(shape);
+
+        for(IShape shape : movedShapes)
+            appState.shapeList.addShape(shape);
+    }
+}
