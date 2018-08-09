2054e6907c1ec4c26522cceda72c78f8f5a59788
Adam Brown <abrow191@mail.depaul.edu>
2018-08-08 19:02:34 -0500

    Add clear canvas functionality prior to shapes being drawn.

--- src/controller/ShapeDrawer.java
+++ src/controller/ShapeDrawer.java
@@ -6,6 +6,12 @@ import java.util.Iterator;
 import java.awt.Graphics2D;
 import model.persistence.ShapeList;
 import model.interfaces.IShape;
+import model.ShapeType;
+import model.ShapeColor;
+import model.ShapeShadingType;
+import model.persistence.ShapeConfiguration;
+import model.persistence.Point;
+import model.persistence.ShapeFactory;
 
 public class ShapeDrawer implements IObserver{
 
@@ -19,6 +25,18 @@ public class ShapeDrawer implements IObserver{
 
     @Override
     public void update() {
+
+        ShapeType st = ShapeType.RECTANGLE;
+        ShapeColor pc = ShapeColor.WHITE;
+        ShapeColor sc = ShapeColor.WHITE;
+        ShapeShadingType sst = ShapeShadingType.FILLED_IN;
+        ShapeConfiguration shapeConfiguration = new ShapeConfiguration(st, pc, sc, sst);
+        Point startPoint = new Point(0, 0);
+        Point endPoint = new Point(800, 1200);
+
+        IShape clearCanvas = ShapeFactory.createRectangle(startPoint, endPoint, shapeConfiguration);
+        clearCanvas.drawShape(graphics2D);
+
         List<IShape> iterableShapeList = shapeList.getIterable();
 
         for(IShape shape : iterableShapeList)
