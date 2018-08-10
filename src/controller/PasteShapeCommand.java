1860aed14c8c5dac8581b8ed4b4d727390da51c0
Adam Brown <abrow191@mail.depaul.edu>
2018-08-10 17:46:29 -0500

    Create PasteShapeCommand.

---
+++ src/controller/PasteShapeCommand.java
@@ -0,0 +1,56 @@
+package controller;
+
+import model.ShapeType;
+import model.interfaces.IApplicationState;
+import model.persistence.Point;
+import model.persistence.ShapeConfiguration;
+import model.persistence.ShapeList;
+import model.persistence.Point;
+import model.persistence.ShapeConfiguration;
+import model.ShapeType;
+import model.persistence.ShapeFactory;
+
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+import model.interfaces.IShape;
+
+public class PasteShapeCommand implements ICommand {
+
+    private IApplicationState appState;
+
+    public PasteShapeCommand(IApplicationState appState){
+        this.appState = appState;
+    }
+
+    @Override
+    public void run() {
+        ShapeList clipboard = appState.getClipboard();
+        List<IShape> clipboardShapes = clipboard.getIterable();
+        List<IShape> shapesToPaste = new ArrayList<IShape>();
+        IShape shapeToPaste = null;
+        Point newStartPoint = new Point(0, 0);
+
+        for (IShape shape : clipboardShapes){
+            int newEndX = (shape.getEndPoint().getX()) - (shape.getStartPoint().getX());
+            int newEndY = (shape.getEndPoint().getY()) - (shape.getStartPoint().getY());
+            Point newEndPoint = new Point(newEndX, newEndY);
+            ShapeConfiguration sc = shape.getShapeConfiguration();
+
+            if (shape.getShapeType() == ShapeType.ELLIPSE)
+                shapeToPaste = ShapeFactory.createEllipse(newStartPoint, newEndPoint, sc);
+            else if (shape.getShapeType() == ShapeType.RECTANGLE)
+                shapeToPaste = ShapeFactory.createRectangle(newStartPoint, newEndPoint, sc);
+            else if (shape.getShapeType() == ShapeType.TRIANGLE)
+                shapeToPaste = ShapeFactory.createTriangle(newStartPoint, newEndPoint, sc);
+
+            shapesToPaste.add(shapeToPaste);
+
+        }
+
+        for (IShape shape : shapesToPaste)
+            appState.addToShapeList(shape);
+
+    }
+
+}
